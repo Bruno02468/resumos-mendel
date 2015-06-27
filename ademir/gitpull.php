c4e048e19e355f919000d5cface52c5bb9e18a22
= <bruno@paschoalinoto.com>
2015-06-26 22:38:53 -0300

    gitpull.php

---
+++ ademir/gitpull.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Editando resumo</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="/stylesheets/dark.css">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    </head>
+    
+    <body>
+        <center>
+        <h1>Site dos Resumos - Sincronizar com o GitHub"</h1>
+        <br>
+        <?php echo exec("cd /home/bruno/Apache/resumos/ && git pull origin master"); ?>
+    </body>
+</html>
