1afa3fb892a54444c84b037fcd585203a6795f68
= <bruno@paschoalinoto.com>
2015-06-28 17:20:46 -0300

    chega de nomes de arquivo

--- ademir/atuadores/cria.php
+++ ademir/atuadores/cria.php
@@ -8,14 +8,29 @@ function req($str) {
     }
 }
 
-$filename = req('filename');
+$pasta = "../../dados/";
+
+function filename($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    if (!file_exists($pasta . $randomString))
+        return $randomString;
+    else
+        return filename();
+}
+
+$filename = filename();
 $titulo = req('titulo');
 $autoria = req('autoria');
 $dados = req('dados');
 
 
 $arquivo = "$titulo\n$autoria\n0\n$dados";
-file_put_contents("../../dados/" . $filename, $arquivo);
+file_put_contents($pasta . $filename, $arquivo);
 
 $host  = $_SERVER['HTTP_HOST'];
 $uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
