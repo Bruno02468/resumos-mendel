8c76120aee199e07989650139cdba771e527c416
= <bruno@paschoalinoto.com>
2015-11-24 17:46:35 -0200

    bom dia css do dia

--- index.php
+++ index.php
@@ -53,6 +53,7 @@ if ($final == "")
         <link rel="stylesheet" type="text/css" href="resumo.css">
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     </head>
 
     <body>
--- resumo.php
+++ resumo.php
@@ -33,6 +33,7 @@ $conteudo = formatar(implode("", $arquivo));
         </style>
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     </head>
 
     <body>
